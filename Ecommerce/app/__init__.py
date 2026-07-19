7253543b12625e19c9f9782dbc9b5d287399e9ef
Farhan-2007 <shaikhmf9322@gmail.com>
2026-07-19 12:51:16 +0530

    User_authentication

--- Ecommerce/app/__init__.py
+++ Ecommerce/app/__init__.py
@@ -1,18 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from app.auth import auth
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
     db.init_app(app)
 
     from app import models
     
     from app.routes import main
+    from app.auth import auth
     app.register_blueprint(main)
     app.register_blueprint(auth)
 
