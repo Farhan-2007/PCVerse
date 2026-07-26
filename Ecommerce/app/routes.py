97393cf24c7b1f1c4ba66f975a8813c4a69366b8
Farhan-2007 <shaikhmf9322@gmail.com>
2026-07-26 17:23:05 +0530

    UI Updated

--- Ecommerce/app/routes.py
+++ Ecommerce/app/routes.py
@@ -5,21 +5,36 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
+    return render_template("home.html")
+
+
+@main.route("/books")
+def books():
     books = Product.query.order_by(Product.name.asc()).all()
     categories = Category.query.all()
-    return render_template("home.html", books = books, categories = categories)
+
+    return render_template(
+        "books.html",
+        books=books,
+        categories=categories
+    )
+
 
 @main.route("/book/<int:book_id>")
 def book(book_id):
     book = Product.query.get_or_404(book_id)
 
-    return render_template("book.html", book = book)
+    return render_template("book.html", book=book)
+
 
 @main.route("/category/<int:category_id>")
 def category_books(category_id):
     category = Category.query.get_or_404(category_id)
 
-    books = Product.query.filter_by(category_id = category_id).all()
+    books = Product.query.filter_by(category_id=category_id).all()
 
-    return render_template("category.html", category =  category, books = books)
-
+    return render_template(
+        "category.html",
+        category=category,
+        books=books
+    )
